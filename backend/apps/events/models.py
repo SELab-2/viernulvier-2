0d5071539347462b0f9a7b3edc32158bfe38360d
Prince-Bert Beya <princebert.beya@ugent.be>
2026-02-20 13:13:09 +0100

    Field-related changes for event model

--- backend/apps/events/models.py
+++ backend/apps/events/models.py
@@ -13,13 +13,23 @@ class Event(BaseModel):
 
     hall = models.ForeignKey(
         Hall, # TODO: implement Hall model
-        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
         db_comment="The hall which the event is organized in.",
         related_name="events",
     )
 
-    starts_at = models.DateTimeField(db_comment="The time at which the event starts.")
-    ends_at = models.DateTimeField(db_comment="The time at which the event ends.")
+    starts_at = models.DateTimeField(
+        db_comment="The time at which the event starts.",
+        null=True,
+        blank=True
+        )
+    ends_at = models.DateTimeField(
+        db_comment="The time at which the event ends.",
+        null=True,
+        blank=True
+        )
 
     ticketing_url = models.URLField(
         db_comment="The URL leading to the ticket reservations.",
