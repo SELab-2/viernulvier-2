952e208a7cfea53d278e1d808574439b187b4ca6
Prince-Bert Beya <princebert.beya@ugent.be>
2026-02-20 12:44:30 +0100

    Implemented EventPrice model (DB)

--- backend/apps/events/models.py
+++ backend/apps/events/models.py
@@ -37,4 +37,44 @@ class Event(BaseModel):
                 name="event_ends_after_starts",
             )
         ]
+
+class EventPrice(BaseModel):
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        db_comment="The event which the price belongs to.",
+        related_name="prices",
+    )
+
+    price_rank = models.ForeignKey(
+        "pricing.PriceRank",
+        on_delete=models.PROTECT,
+        db_comment="The rank corresponding to the price.",
+        related_name="event_prices",
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        db_comment="The price amount.",
+    )
+
+    available = models.PositiveIntegerField(
+        db_comment="The amount of event tickets available for this specific price."
+    )
+
+    class Meta(BaseModel.Meta):
+        db_table = "event_price"
+        verbose_name = "Event Price"
+        verbose_name_plural = "Event Prices"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["event", "price_rank"],
+                name="uniq_event_price_rank",
+            )
+        ]
+        indexes = [
+            models.Index(fields=["event"]),
+            models.Index(fields=["event", "price_rank"]),
+        ]
         
