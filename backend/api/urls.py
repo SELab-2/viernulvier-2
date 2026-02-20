0608ad5f0db3d808a39fedff80cd77f83aa6b343
Elias Ryckaert <elias.ryckaert@telenet.be>
2026-02-20 12:09:05 +0100

    Add api urls (in comment)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,9 +7,21 @@ from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 # Create a router and register our viewsets with it.
-router = DefaultRouter() 
+router = DefaultRouter()
+
+# TODO register viewsets here
+#router.register(r'productions', ProductionViewSet, basename='production')
+#router.register("events", EventViewSet, basename="event")
+#router.register("locations", LocationViewSet, basename="location")
+#router.register("halls", HallViewSet, basename="hall")
+#router.register("genres", GenreViewSet, basename="genre")
+#router.register("tags", TagViewSet, basename="tag")
+#router.register("prices", PriceViewSet, basename="price")
+#router.register("price-ranks", PriceRankViewSet, basename="price-rank")
+#router.register("import-logs", ImportLogViewSet, basename="import-log")
 
 urlpatterns = [
     path('', include(router.urls)), # Include the router URLs
     path('schema/', SpectacularAPIView.as_view(), name='schema'), # API schema view
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), # API documentation view
+]
