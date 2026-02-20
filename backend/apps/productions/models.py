79ecddf09838338ddc736c9ef5a415141acf0f66
Elias Ryckaert <elias.ryckaert@telenet.be>
2026-02-20 13:30:54 +0100

    Add uit database theme model

--- backend/apps/productions/models.py
+++ backend/apps/productions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from apps.core.model import BaseModel
 
-# Create your models here.
+class UitDatabaseTheme(BaseModel):
+    """Model representing a theme in the UIT database."""
+    name = models.CharField(
+        max_length=200,
+        db_comment="The name of the theme."
+    )
+
+    class Meta(BaseModel.Meta):
+        db_table = "uit_database_theme"
+        verbose_name = "UIT Database Theme"
+        verbose_name_plural = "UIT Database Themes"
+
+    def __str__(self):
+        return self.name
