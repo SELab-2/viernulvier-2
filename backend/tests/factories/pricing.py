e5abe1357df333417110d17301896ffc77720715
Prince-Bert Beya <princebert.beya@ugent.be>
2026-02-21 22:40:42 +0100

    Rectified pricing.py for more stable tests

--- backend/tests/factories/pricing.py
+++ backend/tests/factories/pricing.py
@@ -36,8 +36,8 @@ class PriceRankFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = PriceRank
 
-    position = factory.Faker('random_int', min=0, max=10)
-    sold_out_buffer = factory.Faker('random_int', min=0, max=10)
+    position = factory.Sequence(lambda n: n + 1)
+    sold_out_buffer = 0
 
 
 class PriceRankTranslationFactory(factory.django.DjangoModelFactory):
