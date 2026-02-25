16455a6811abb116616b56a8bb632d1caea7caf7
Prince-Bert Beya <princebert.beya@ugent.be>
2026-02-25 14:03:35 +0100

    Implemented viewsets corresponding to pricing

---
+++ backend/apps/pricing/views.py
@@ -0,0 +1,66 @@
+from django.db.models import Prefetch
+from rest_framework import mixins
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from apps.core.views import ApiModelViewSet
+from apps.pricing.models import (
+    Price,
+    PriceTranslation,
+    PriceRank,
+    PriceRankTranslation,
+)
+from apps.pricing.serializers import (
+    PriceSerializer,
+    PriceTranslationSerializer,
+    PriceRankSerializer,
+    PriceRankTranslationSerializer
+)
+
+
+class PriceViewSet(ApiModelViewSet):
+    """Prices endpoint."""
+    serializer_class = PriceSerializer
+    queryset = (
+        Price.objects.all()
+        .prefetch_related(
+            Prefetch(
+                "translations",
+                queryset=PriceTranslation.objects.select_related("language"),
+            )
+        )
+        .order_by("sort_order", "id")
+    )
+
+
+class PriceTranslationViewSet(ApiModelViewSet):
+    """PriceTranslations endpoint."""
+    serializer_class = PriceTranslationSerializer
+    queryset = (
+        PriceTranslation.objects.all()
+        .select_related("price", "language")
+        .order_by("price_id", "language_id", "id")
+    )
+
+
+class PriceRankViewSet(ApiModelViewSet):
+    """PriceRanks endpoint."""
+    serializer_class = PriceRankSerializer
+    queryset = (
+        PriceRank.objects.all()
+        .prefetch_related(
+            Prefetch(
+                "translations",
+                queryset=PriceRankTranslation.objects.select_related("language"),
+            )
+        )
+        .order_by("id")
+    )
+
+
+class PriceRankTranslationViewSet(ApiModelViewSet):
+    """PriceRankTranslations endpoint."""
+    serializer_class = PriceRankTranslationSerializer
+    queryset = (
+        PriceRankTranslation.objects.all()
+        .select_related("price_rank", "language")
+        .order_by("price_rank_id", "language_id", "id")
+    )
