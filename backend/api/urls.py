816b48fd2d40856b032292c58e4dbe460f7b2aca
Elias Ryckaert <elias.ryckaert@ugent.be>
2026-02-25 14:28:53 +0100

    Add to urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,18 +6,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from apps.languages.views import LanguageViewSet
+from apps.tags.views import TagViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 
 # TODO register viewsets here
 router.register(r'languages', LanguageViewSet, basename='language')
+router.register(r'tags', TagViewSet, basename='tag')
 #router.register(r'productions', ProductionViewSet, basename='production')
 #router.register("events", EventViewSet, basename="event")
 #router.register("locations", LocationViewSet, basename="location")
 #router.register("halls", HallViewSet, basename="hall")
 #router.register("genres", GenreViewSet, basename="genre")
-#router.register("tags", TagViewSet, basename="tag")
 #router.register("prices", PriceViewSet, basename="price")
 #router.register("price-ranks", PriceRankViewSet, basename="price-rank")
 #router.register("import-logs", ImportLogViewSet, basename="import-log")
