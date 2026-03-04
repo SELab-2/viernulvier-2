b89f430ba70dd6b2603b01b4d468a4fda78ec554
Arne Maegherman <arne.maegherman@gmail.com>
2026-03-04 19:32:08 +0100

    formatting

--- backend/apps/productions/models.py
+++ backend/apps/productions/models.py
@@ -184,12 +184,14 @@ class ProductionTranslation(BaseModel):
     )
 
     video_1 = models.URLField(
-        db_comment="The URL of the first video of the production in the given language.",
+        db_comment="The URL of the first video of the production \
+            in the given language.",
         blank=True,
     )
 
     video_2 = models.URLField(
-        db_comment="The URL of the second video of the production in the given language.",
+        db_comment="The URL of the second video of the production \
+            in the given language.",
         blank=True,
     )
 
@@ -258,7 +260,8 @@ class ProductionGenre(BaseModel):
     )
 
     position = models.PositiveIntegerField(
-        db_comment="The position of the genre in the list of genres for the production.",
+        db_comment="The position of the genre \
+            in the list of genres for the production.",
         # default=0, # TODO in viewset ordering definieren
     )
 
