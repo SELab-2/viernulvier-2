ad29a03edad3207a8a89730df48fc03a9e91d5c4
Prince-Bert Beya <princebert.beya@ugent.be>
2026-03-05 12:46:07 +0100

    Test commit

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -104,7 +104,7 @@ DATABASES = {
 }
 
 
-# Password validation
+# Password validation 
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
