b82556949e98ee4b66e5063a08c0dd7124354bff
Floko2005 <f.mataigne@gmail.com>
2026-03-07 15:12:28 +0100

    ruff fixes

--- backend/apps/languages/admin.py
+++ backend/apps/languages/admin.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 from apps.core.admin import BaseAdmin
 from .models import Language
 
-from apps.core.admin import BaseAdmin
-from .models import Language
 
 
 @admin.register(Language)
