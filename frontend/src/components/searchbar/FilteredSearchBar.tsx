2fb4c625ca3f9341b83acb39ab71f078c11a78f1
Daan Phlips <daan.phlips@ugent.be>
2026-03-20 21:16:45 +0100

    search button

--- frontend/src/components/searchbar/FilteredSearchBar.tsx
+++ frontend/src/components/searchbar/FilteredSearchBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import DropDownFilter, { DropDownFilterProps } from './filters/DropDownFilter'
 import TagList from './tags/TagList'
 import LayoutOptionList from './layout_options/LayoutOptionList'
 import SearchBar from './SearchBar'
+import { useTranslation } from 'react-i18next'
 
 // This represents the entire search bar component, which includes the search input, filter dropdowns, tag list, and layout options.
 export interface FilteredSearchBarProps {
@@ -17,6 +18,7 @@ export interface FilteredSearchBarProps {
   layoutOptions: { name: string; displayName: string }[]
   currentLayout: string
   onLayoutChange: (layout: string) => void
+  onSearchSubmit?: () => void
 }
 
 // This component renders the search bar with all its functionalities based on the provided props.
@@ -32,6 +34,8 @@ const FilteredSearchBar: React.FC<FilteredSearchBarProps> = ({
   currentLayout: currentLayout,
   onLayoutChange: onLayoutChange,
 }) => {
+  const t = useTranslation().t
+
   return (
     <Box>
       <Box display="flex" gap={2} alignItems="center">
@@ -43,14 +47,24 @@ const FilteredSearchBar: React.FC<FilteredSearchBarProps> = ({
         {filters.map((filter) => (
           <DropDownFilter key={filter.name} {...filter} />
         ))}
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: '#8224E3FF',
+          }}
+        >
+          {t('searchbar.search')}
+        </Button>
       </Box>
       <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
         <TagList tags={tags} selectedTags={selectedTags} onTagToggle={onTagToggle} />
-        <LayoutOptionList
-          layout_options={layoutOptions}
-          selected_layout={currentLayout}
-          onLayoutChange={onLayoutChange}
-        />
+        {layoutOptions.length > 1 && (
+          <LayoutOptionList
+            layout_options={layoutOptions}
+            selected_layout={currentLayout}
+            onLayoutChange={onLayoutChange}
+          />
+        )}
       </Box>
     </Box>
   )
