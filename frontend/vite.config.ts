af50f4645aebadfae5162a5cb6a95d2be406d327
Jasper Delmeiren <jasper.delmeiren@ugent.be>
2026-04-08 11:28:52 +0200

    Undo accidental changes to vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,42 +9,23 @@ export default ({ mode }) => {
     define: {
       'process.env.VITE_PUBLIC_API_KEY': JSON.stringify(env.VITE_PUBLIC_API_KEY ?? ''),
     },
-    // server: {
-    //   proxy: {
-    //     '/api': {
-    //       target: 'http://localhost:8000',
-    //       changeOrigin: true,
-    //     },
-    //     '/admin': {
-    //       target: 'http://localhost:8000',
-    //       changeOrigin: true,
-    //     },
-    //     '/static': {
-    //       target: 'http://localhost:8000',
-    //       changeOrigin: true,
-    //     },
-    //     '/media': {
-    //       target: 'http://localhost:8000',
-    //       changeOrigin: true,
-    //     },
-    //   },
-    // },
     server: {
       proxy: {
         '/api': {
-          target: 'https://sel2-2.ugent.be',
+          target: 'http://localhost:8000',
           changeOrigin: true,
-          secure: true,
         },
-        '/media': {
-          target: 'https://sel2-2.ugent.be',
+        '/admin': {
+          target: 'http://localhost:8000',
           changeOrigin: true,
-          secure: true,
         },
         '/static': {
-          target: 'https://sel2-2.ugent.be',
+          target: 'http://localhost:8000',
           changeOrigin: true,
-          secure: true,
+        },
+        '/media': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
         },
       },
     },
