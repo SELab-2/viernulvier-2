3bb808275b6080c6ea75736c8945656b280e3526
Noah Delvoye <noah.delvoye@hotmail.be>
2026-04-08 10:15:40 +0200

    remove duplicate return
    Co-authored-by: Elias Ryckaert <152428334+eliaryck@users.noreply.github.com>

--- frontend/src/components/chips/genreAndTagChipStyles.ts
+++ frontend/src/components/chips/genreAndTagChipStyles.ts
@@ -78,16 +78,6 @@ export const getGenreAndTagChipStyles = ({
     }
   }
 
-  if (theme.palette.mode === 'light') {
-    return {
-      ...base,
-      backgroundColor: theme.palette.background.paper,
-      color: theme.palette.text.primary,
-      borderColor: theme.palette.text.primary,
-      ...unselectedHoverStyles,
-    }
-  }
-
   return {
     ...base,
     backgroundColor: theme.palette.background.paper,
