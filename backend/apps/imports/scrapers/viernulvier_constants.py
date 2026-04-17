851d143f960a6d1b96fe945cf3fae7ac6fe499c3
Florian Mataigne <111384163+Floko2005@users.noreply.github.com>
2026-04-17 19:24:40 +0200

    Update backend/apps/imports/scrapers/viernulvier_constants.py
    Co-authored-by: Elias Ryckaert <152428334+eliaryck@users.noreply.github.com>

--- backend/apps/imports/scrapers/viernulvier_constants.py
+++ backend/apps/imports/scrapers/viernulvier_constants.py
@@ -81,6 +81,7 @@ class M2MConfig:
     related_lookup_field: str = "external_id"
     extra_fields: dict[str, str] = field(default_factory=dict)
     create_related_fn: Callable[[str, Any], Any | None] | None = None
+    clear_existing: bool = True
 
 
 @dataclass
