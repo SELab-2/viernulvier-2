ee432a1c9ac09353c0edd2d341dc636f5d72cfad
Prince-Bert Beya <princebert.beya@ugent.be>
2026-05-03 09:12:27 +0200

    Env fix

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -12,7 +12,10 @@ Usage (via pytest.ini):
     DJANGO_SETTINGS_MODULE = config.settings.test
 """
 
+import os
 import tempfile
+
+os.environ.setdefault("REDIS_URL", "redis://127.0.0.1:6379/1")
 
 from .base import *  # noqa: F403
 from .base import REST_FRAMEWORK
