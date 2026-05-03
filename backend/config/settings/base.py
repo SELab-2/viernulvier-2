5f281a8cefbab4b3076c39c2329937a83c015b91
Prince-Bert Beya <princebert.beya@ugent.be>
2026-05-03 08:50:39 +0200

    Redis backend fix

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -361,9 +361,10 @@ LOGGING = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "LOCATION": os.environ["REDIS_URL"],
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
         },
         "KEY_PREFIX": "viernulvier",
     }
