d10818edf451d2e827d6eb0bf59a4fb786edc494
Noah Delvoye <noah.delvoye@hotmail.be>
2026-05-06 21:57:31 +0200

    feat: add NotificationProvider to BlogsPage and ProductionsPage tests for alert management

--- frontend/src/__tests__/pages/ProductionsPage.test.tsx
+++ frontend/src/__tests__/pages/ProductionsPage.test.tsx
@@ -3,6 +3,7 @@ import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter, useLocation } from 'react-router-dom'
 
+import { NotificationProvider } from '../../contexts/NotificationContext'
 import i18n from '../../i18n'
 import ProductionsPage from '../../pages/ProductionsPage'
 import { ApiError } from '../../services/ApiTypes'
@@ -119,8 +120,10 @@ const renderPage = (
     <MemoryRouter initialEntries={[initialEntry]}>
       <I18nextProvider i18n={i18n}>
         <ThemeProvider theme={createTheme()}>
-          <ProductionsPage />
-          <LocationProbe />
+          <NotificationProvider>
+            <ProductionsPage />
+            <LocationProbe />
+          </NotificationProvider>
         </ThemeProvider>
       </I18nextProvider>
     </MemoryRouter>,
